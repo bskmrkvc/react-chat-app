867a102e38622765923477321831e101a859268c
unknown <markovic.bosko2000@gmail.com>
2022-01-24 20:25:48 +0100

    Updates

---
+++ precache-manifest.8756529e9f3206932cadcb99110ed525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec08e5800676c9186948853c67d9816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c420fe458661f85581ba",
+    "url": "/static/css/main.55f85a97.chunk.css"
+  },
+  {
+    "revision": "123065fe5924a6495e73",
+    "url": "/static/js/2.a6f50e4d.chunk.js"
+  },
+  {
+    "revision": "bd1dc215f38c93919c406d32014fca4d",
+    "url": "/static/js/2.a6f50e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c420fe458661f85581ba",
+    "url": "/static/js/main.1e1567ab.chunk.js"
+  },
+  {
+    "revision": "dc7457b66b5e09406a87",
+    "url": "/static/js/runtime-main.8ecbaadf.js"
+  }
+]);
